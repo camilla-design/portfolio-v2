8933ff1da3465c510b735fb3e6a9dbf79138c6f6
Atlungstad Brenneri <camilla_repp@hotmail.com>
2021-06-24 16:25:40 +0200

    change cards

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -13,59 +13,54 @@ class Project extends Component {
             <hr></hr>
             <div className="project-container">
               <div className="project-content">
-                <h3>Heels Shop</h3>
                 <img src={heelsImg} alt="Heels shop homescreen" />
-                <div className="project-links">
-                  <a href="#">Github</a>
-                  <a href="#">Besøk side</a>
-                </div>
+                <h3>Heels Shop</h3>
                 <p>
                   Heels er en e-commerce nettside bygd på Next.js, hvor Strapi
                   har blitt brukt for å holde all api.
                 </p>
                 <p>
                   <span>
-                    | Next.js | CSS Module | Strapi.IO | Amazone AWS | Heroku |
-                    Vercel | MongoDB | Formspree | Animasjon |
+                    | Next.js | CSS Module |
                   </span>
                 </p>
+                <div className="project-links">
+                  <a href="#">Les mer</a>
+                </div>
               </div>
             </div>
             <div className="project-container">
               <div className="project-content">
-                <h3>Atlungstad Brenner</h3>
                 <img src={atlungstadImg} alt="Atlungstad Brenneri homescreen" />
-                <div className="project-links">
-                  <a href="#">Github</a>
-                  <a href="#">Besøk side</a>
-                </div>
+                <h3>Atlungstad Brenner</h3>
                 <p>
                   Atlungstad Brenneri er en restaurant nettside bygd på
                   React.js, hvor Strapi har blitt brukt for å holde all api.
                 </p>
                 <p>
                   <span>
-                    | React.js | Sass | Bootstrap | Strapi.IO | Amazon AWS |
-                    Heroku | Vercel | MongoDB | Formspree | Animasjon |
+                    | React.js | Sass | Bootstrap | 
                   </span>
                 </p>
+                <div className="project-links">
+                  <a href="#">Les mer</a>
+                </div>
               </div>
             </div>
             <div className="project-container">
               <div className="project-content">
-                <h3>EarProof</h3>
                 <img src={earproofImg} alt="EarProof homescreen" />
-                <div className="project-links">
-                  <a href="#">Github</a>
-                  <a href="#">Besøk side</a>
-                </div>
+                <h3>EarProof</h3>
                 <p>
                   EarProof er en e-comers webside, bygd på Vanilla JavaScript,
                   Sass og HTML. Strapi er brukt for å holde all API.
                 </p>
                 <p>
-                  <span>| JavaScript | Sass | HTML | Strapi |</span>
+                  <span>| JavaScript | Sass | HTML | </span>
                 </p>
+                <div className="project-links">
+                  <a href="#">Les mer</a>
+                </div>
               </div>
             </div>
           </div>
